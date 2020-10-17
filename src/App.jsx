633228ecac7d5256bf3f8882e87abbc92c9f1f6d
Joshua Sledden <JoshuaSledden@gmail.com>
2020-10-17 17:38:38 +0100

    chore: app refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,16 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
-import logo from './logo.svg';
 import './App.css';
 import { useSelector } from 'react-redux';
 
 const theme = useSelector(state => state.theme);
 
-function App() {
-  const appContent = (
-    <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.jsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </>
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <p>Hello, World!</p>
+    </ThemeProvider>
   );
-
-  return <ThemeProvider theme={theme}>{appContent}</ThemeProvider>;
-}
+};
 
 export default App;
