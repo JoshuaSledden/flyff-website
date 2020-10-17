46fb51be16829164ce83e0c708f2147451e69d42
Joshua Sledden <JoshuaSledden@gmail.com>
2020-10-17 21:57:27 +0100

    feat: basic navigation bar template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { useSelector } from 'react-redux';
+import NavigationBar from './components/navigation-bar';
 
 const App = () => {
   const { theme } = useSelector(state => state.themeReducer);
   return (
     <ThemeProvider theme={theme}>
-      <p>Hello, World!</p>
+      <NavigationBar />
     </ThemeProvider>
   );
 };
