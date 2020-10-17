38392a2ad9ee678f5223fd6c2552b1be1cee6cfc
Joshua Sledden <JoshuaSledden@gmail.com>
2020-10-17 14:42:35 +0100

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import logo from './logo.svg';
+import './App.css';
+
+const theme = {
+  main: 'mediumseagreen'
+};
+
+function App() {
+  const appContent = (
+    <>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.jsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
+  );
+
+  return <ThemeProvider theme={theme}>{appContent}</ThemeProvider>;
+}
+
+export default App;
