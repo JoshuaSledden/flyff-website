f8eb8acd998e60f8a41377d37c56b434ecf390b9
Joshua Sledden <JoshuaSledden@gmail.com>
2020-10-18 19:34:17 +0100

    feat: placeholder background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import styled, { ThemeProvider } from 'styled-components';
 import { useSelector } from 'react-redux';
 import Routes from './routes';
 import NavigationBar from './components/navigation-bar';
+import BackgroundImage from './components/images/placeholder.jpg';
+
+const Background = styled.div`
+  position: fixed;
+  background-image: url(${BackgroundImage});
+  background-repeat: no-repeat;
+  height: 100vh;
+  width: 100%;
+  z-index: -1;
+`;
 
 const App = () => {
   const { theme } = useSelector(state => state.themeReducer);
   return (
     <ThemeProvider theme={theme}>
+      <Background />
       <NavigationBar />
       <Routes />
     </ThemeProvider>
