3559bd9295bf6f2cb991218c5b6b022bf8532587
Joshua Sledden <JoshuaSledden@gmail.com>
2020-11-06 19:33:59 +0000

    fix: connected router issue

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createBrowserHistory } from 'history';
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import { routerMiddleware } from 'connected-react-router';
 import reducers from './reducers';
 
@@ -7,5 +7,5 @@ export const history = createBrowserHistory();
 
 export default createStore(
   reducers(history),
-  applyMiddleware(routerMiddleware(history))
+  compose(applyMiddleware(routerMiddleware(history)))
 );
